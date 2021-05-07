541fdb6e35cb03b2e5310c181e4f0c2c1c2cfba6
Ernest Ndungu <ernjohn@microsoft.com>
2021-05-07 09:58:25 +0300

    Add project files.

---
+++ DevopsDemoProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DevopsDemoProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
